fe05ccabb05af0d385cb1f39327b75cfc20ed6ff
ACH90 <chassagne.alexandre@gmail.com>
2025-03-10 14:06:52 +0100

    Ex5 ok & Cypress ScreenShots

--- Exercice6/index.js
+++ Exercice6/index.js
@@ -12,7 +12,11 @@ const clearDisplay = () => {
 const calculateResult = () => {
   const display = document.getElementById("display");
   try {
-    display.value = eval(display.value);
+    if (display.value.includes("/0")) {
+      display.value = "Division by zero is not allowed";
+    } else {
+      display.value = eval(display.value);
+    }
   } catch (error) {
     display.value = "Error";
   }
